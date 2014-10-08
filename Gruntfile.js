8c5cc0e3ee9c72cdb2fb86de7a3d4e1dbf796af3
Rich C <frobitz@gmail.com>
2014-10-08 17:28:57 +0100

    Set up Go

---
+++ Gruntfile.js
@@ -0,0 +1,97 @@
+module.exports = function(grunt) {
+
+  // Load all grunt tasks
+  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+  // Project configuration.
+  grunt.initConfig({
+
+    watch: {
+      sass: {
+        files: 'src/sass/**/*.scss',
+        tasks: ['sass']
+      },
+      jade: {
+        files: 'src/jade/**/*.jade',
+        tasks: ['jade']
+      },
+      imagemin: {
+        files: 'src/img/**/*.{gif,png,jpg}',
+        tasks: ['imagemin']
+      }
+    },
+
+    sass: {
+      dist: {
+        options: {
+          sourcemap: true,
+          style: 'compressed'
+        },
+        files: {
+          'dist/_/styles/screen.css': 'src/sass/screen.scss'
+        }
+      }
+    },
+
+    jade: {
+      compile: {
+        options: {
+          pretty: true
+        },
+        files: [
+          {
+            dest: 'dist/',
+            expand: true,
+            ext: '.html',
+            flatten: true,
+            src: ['src/jade/**/*.jade', '!src/jade/includes/*.jade']
+          }
+        ]
+      }
+    },
+
+    imagemin: {
+      gif: {
+        files: [
+          {
+            dest: 'dist/_/images/gif/',
+            expand: true,
+            ext: '.gif',
+            src: ['src/img/gif/*.gif']
+          }
+        ]
+      },
+      png: {
+        options: {
+          optimizationLevel: 7
+        },
+        files: [
+          {
+            dest: 'dist/_/images/png/',
+            expand: true,
+            ext: '.png',
+            src: ['src/img/png/*.png']
+          }
+        ]
+      },
+      jpg: {
+        options: {
+          progressive: true
+        },
+        files: [
+          {
+            dest: 'dist/_/images/jpg/',
+            expand: true,
+            ext: '.jpg',
+            flatten: true,
+            src: ['src/img/jpg/*.jpg'],
+          }
+        ]
+      }
+    }
+
+  });
+
+  grunt.registerTask('default', ['watch']);
+
+};
