d69e0944cd8dda1dad72bdefd9b11c13c5c832de
Rich Cookson <rich.cookson@gmail.com>
2018-01-31 22:55:21 +0000

    Added browsersync package

--- gulpfile.js
+++ gulpfile.js
@@ -3,5 +3,6 @@ const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
 const concat = require('gulp-concat');
 const nodemon = require('gulp-nodemon');
+const browserSync = require('browser-sync').create();
 
 gulp.task('default');
