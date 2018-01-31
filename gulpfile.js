98181b4193ec938e4cd2ea314fe0db2369853766
Rich Cookson <rich.cookson@gmail.com>
2018-01-31 23:20:38 +0000

    Added Bourbon and Neat node packages

--- gulpfile.js
+++ gulpfile.js
@@ -4,22 +4,25 @@ const autoprefixer = require('gulp-autoprefixer');
 const concat = require('gulp-concat');
 const nodemon = require('gulp-nodemon');
 const browserSync = require('browser-sync').create();
+const bourbon = require('bourbon').includePaths;
+const neat = require('bourbon-neat').includePaths;
 
 const path = {
-  'styles' : {
-    'dest' : 'dist/css',
-    'src' : 'src/sass/**/*.scss'
+  'styles': {
+    'dest': 'dist/css',
+    'src': 'src/sass/**/*.scss'
   },
-  'scripts' : {
-    'dest' : 'public/js',
-    'src' : 'src/js/*.js'
+  'scripts': {
+    'dest': 'public/js',
+    'src': 'src/js/*.js'
   }
 };
 
 gulp.task('sass', function() { 
   return gulp.src(path.styles.src)
     .pipe(sass({
-      outputStyle: 'compressed'
+      outputStyle: 'compressed',
+      includePaths: [bourbon, neat]
     }))
     .pipe(autoprefixer()) 
     .pipe(concat('styles.css'))
