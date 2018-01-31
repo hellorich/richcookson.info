8fd028d47b85add8b8e771eefc66798cf1fd2a2a
Rich Cookson <rich.cookson@gmail.com>
2018-01-31 22:53:29 +0000

    Added gulp nodemon package

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
+const concat = require('gulp-concat');
+const nodemon = require('gulp-nodemon');
 
 gulp.task('default');
