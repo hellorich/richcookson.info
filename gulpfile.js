cba2a9541a0001170580b67c29eb411367c8fcce
Rich Cookson <rich.cookson@gmail.com>
2018-01-31 23:05:48 +0000

    Added path information to Gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -5,4 +5,15 @@ const concat = require('gulp-concat');
 const nodemon = require('gulp-nodemon');
 const browserSync = require('browser-sync').create();
 
+const path = {
+  'styles' : {
+    'dest' : 'dist/css',
+    'src' : 'src/sass/**/*.scss'
+  },
+  'scripts' : {
+    'dest' : 'public/js',
+    'src' : 'src/js/*.js'
+  }
+};
+
 gulp.task('default');
