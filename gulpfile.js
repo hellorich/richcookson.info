6c1bbbdc3bdd7a564ae9aebc2bb32e052ca644d9
Rich Cookson <richard.cookson@brilliantnoise.com>
2018-02-01 12:41:57 +0000

    Added handlebars support for HTML and Imagemin task

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
-const handlebars = require('gulp-handlebars');
 const autoprefixer = require('gulp-autoprefixer');
+const handlebars = require('gulp-compile-handlebars');
+const imagemin = require('gulp-imagemin');
 const concat = require('gulp-concat');
+const rename = require('gulp-rename');
 const nodemon = require('gulp-nodemon'); // ?
 const browserSync = require('browser-sync').create();
 const bourbon = require('bourbon').includePaths;
@@ -20,12 +22,16 @@ const path = {
   'scripts': {
     'dest': 'dist/assets/js',
     'src': 'src/js/*.js'
+  },
+  'images': {
+    'dest': 'dist/assets/images',
+    'src': 'src/img/**/*.{gif,png,jpg,svg}'
   }
 };
 
 // Task: SASS
-gulp.task('sass', function() { 
-  return gulp.src(path.styles.src)
+gulp.task('sass', () => { 
+  gulp.src(path.styles.src)
     .pipe(sass({
       outputStyle: 'compressed',
       includePaths: [bourbon, neat]
@@ -36,20 +42,32 @@ gulp.task('sass', function() { 
 });
 
 // Task: Handlebars
-gulp.task('handlebars', function(){
-  gulp.src(path.html.src)
-    .pipe(handlebars())
-    .pipe(gulp.dest(path.styles.html));
+gulp.task('handlebars', () => {
+  gulp.src('src/hbs/index.hbs')
+    .pipe(handlebars({}, {
+      batch : ['./src/hbs/partials']
+    }))
+    .pipe(rename('index.html'))
+    .pipe(gulp.dest(path.html.dest));
 });
 
+// Task: ImageMin
+gulp.task('imagemin', () =>
+  gulp.src(path.images.src)
+    .pipe(imagemin())
+    .pipe(gulp.dest(path.images.dest))
+);
+// Task: Uglify
+
 // Task: BrowserSync
-gulp.task('go', ['sass'], function() {
+gulp.task('go', ['sass', 'handlebars'], function() {
 
     browserSync.init({
-        server: path.html
+        server: path.html.dest
     });
 
     gulp.watch(path.styles.src, ['sass']);
+    gulp.watch(path.html.src, ['handlebars']);
     gulp.watch(path.html.src).on('change', browserSync.reload);
 });
 
