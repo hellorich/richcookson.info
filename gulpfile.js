c154ab48a46d6db7d110d9ce6385a1b588ec53a2
Rich Cookson <rich.cookson@gmail.com>
2018-02-01 09:39:04 +0000

    Added initial handlebars task

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,29 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const handlebars = require('gulp-handlebars');
 const autoprefixer = require('gulp-autoprefixer');
 const concat = require('gulp-concat');
-const nodemon = require('gulp-nodemon');
+const nodemon = require('gulp-nodemon'); // ?
 const browserSync = require('browser-sync').create();
 const bourbon = require('bourbon').includePaths;
 const neat = require('bourbon-neat').includePaths;
 
 const path = {
+  'html': {
+    'dest': 'dist',
+    'src': 'src/hbs/**/*.hbs'
+  },
   'styles': {
-    'dest': 'dist/css',
+    'dest': 'dist/assets/css',
     'src': 'src/sass/**/*.scss'
   },
   'scripts': {
-    'dest': 'public/js',
+    'dest': 'dist/assets/js',
     'src': 'src/js/*.js'
   }
 };
 
+// Task: SASS
 gulp.task('sass', function() { 
   return gulp.src(path.styles.src)
     .pipe(sass({
@@ -29,4 +35,23 @@ gulp.task('sass', function() { 
     .pipe(gulp.dest(path.styles.dest));
 });
 
-gulp.task('default', ['sass']);
+// Task: Handlebars
+gulp.task('handlebars', function(){
+  gulp.src(path.html.src)
+    .pipe(handlebars())
+    .pipe(gulp.dest(path.styles.html));
+});
+
+// Task: BrowserSync
+gulp.task('go', ['sass'], function() {
+
+    browserSync.init({
+        server: path.html
+    });
+
+    gulp.watch(path.styles.src, ['sass']);
+    gulp.watch(path.html.src).on('change', browserSync.reload);
+});
+
+// Task: Default
+gulp.task('default', ['go']);
