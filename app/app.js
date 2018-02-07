7fb875fb8a3b99c11711d989b9b5795ac50796f8
Rich Cookson <rich.cookson@gmail.com>
2018-02-07 21:31:57 +0000

    Removed some unnecessary code

--- app/app.js
+++ app/app.js
@@ -3,17 +3,13 @@ const express = require('express');
 const helmet = require('helmet');
 const compression = require('compression');
 const app = express();
-const port = process.env.PORT || 5002;
+const port = 5002;
 
 app.use(sslRedirect());
 app.use(helmet());
 app.use(compression());
 
 app.use(express.static('./dist'));
-
-app.get('/', function(req, res) {
-  res.sendfile('./dist/index.html');
-});
 
 app.listen(port, function() {
   console.log('Listening on port:' + port);
