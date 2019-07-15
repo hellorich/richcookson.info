6dd322a18aa85d4d048648a26b70ab32daf5002c
Rich Cookson <rich.cookson@gmail.com>
2019-07-15 10:02:24 +0100

    Added icons back - functioning top button

--- src/js/app.js
+++ src/js/app.js
@@ -4,8 +4,11 @@
 
 //observer.observe(element)
 
+//const html = document.querySelector('html')
 const button = document.querySelector('.button-top')
 const controller = document.querySelector('.top')
+
+//html.classList.add('js')
 
 const observer = new IntersectionObserver((entries) => {
   entries.forEach(entry => {
