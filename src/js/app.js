9ccd8d6bc0dc8bbfb61dda69c383fbb5b887daa1
Rich Cookson <rich.cookson@gmail.com>
2019-11-01 09:24:34 +0000

    Made JS modular - ran npm audit fix

--- src/js/app.js
+++ src/js/app.js
@@ -1,23 +1,5 @@
-//import { observer } from './modules/top'
+import { observer } from './modules/top'
 
-//const element = document.querySelector('.section-skills')
-
-//observer.observe(element)
-
-//const html = document.querySelector('html')
-const button = document.querySelector('.button-top')
 const controller = document.querySelector('.top')
 
-//html.classList.add('js')
-
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      button.classList.remove('display')
-    } else {
-      button.classList.add('display')
-    }
-  })
-})
-
-observer.observe(controller);
+observer.observe(controller)
--- src/js/modules/top.js
+++ src/js/modules/top.js
@@ -1,11 +1,10 @@
-// export const observer = new IntersectionObserver((entry, observer) => {
-//
-//   if (entry.isIntersecting) {
-//     console.log('in the view')
-//     entry.target.classList.add('display')
-//   }
-//   // else {
-//   //   console.log('out of view')
-//   //   entry.target.classList.remove('display')
-//   // }
-// })
+export const observer = new IntersectionObserver((entries) => {
+  const button = document.querySelector('.button-top')
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      button.classList.remove('display')
+    } else {
+      button.classList.add('display')
+    }
+  })
+})
