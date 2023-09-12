601d11e00cabebaef20b41bc22b925c8e28933ba
Rich Cookson <hellorich@frobitz.com>
2023-09-12 19:48:29 +0100

    Added Header component

---
+++ app/_components/header/header.stories.ts
@@ -0,0 +1,18 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import Header from './header'
+
+const meta = {
+  component: Header,
+  title: 'Header',
+} satisfies Meta<typeof Header>
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  args: {
+    title: 'Rich Cookson',
+  },
+}
