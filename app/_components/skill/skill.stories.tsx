d68aa60e8ae52e6e933ea6abea1f4c70bb45b9c4
Rich Cookson <rich.cookson@wearetilt.com>
2023-09-26 11:14:11 +0100

    Set up basic interaction test

--- app/_components/skill/skill.stories.tsx
+++ app/_components/skill/skill.stories.tsx
@@ -1,4 +1,6 @@
+import { expect } from '@storybook/jest'
 import type { Meta, StoryObj } from '@storybook/react'
+import { within, userEvent } from '@storybook/testing-library'
 
 import Skill from './skill'
 
@@ -21,5 +23,22 @@ type Story = StoryObj<typeof Skill>;
 export const Default: Story = {
   args: {
     label: 'Tag',
+		url: 'http://www.google.com',
   },
+}
+
+export const ClickedLink: Story = {
+	args: {
+    label: 'Tag',
+		url: 'http://www.google.com',
+  },
+
+	play: async ({ canvasElement }) => {
+		const canvas = within(canvasElement)
+		const link = canvas.getByRole('link', { name: 'Tag' })
+		
+		await userEvent.click(link)
+
+		await expect(window.location.href).toBe(link.href)
+	}
 }
--- app/_components/skill/skill.tsx
+++ app/_components/skill/skill.tsx
@@ -1,14 +1,17 @@
+import Link from 'next/link'
+
 import { lato } from '../../_utilities/fonts'
 
 import styles from './skill.module.scss'
 
 interface SkillProps {
   label: string,
+	url: string,
 }
 
-const Skill = ({ label, ...props} : SkillProps) => {
+const Skill = ({ label, url, ...props} : SkillProps) => {
   return (
-    <li className={`${styles.skill} ${lato.className}`}><a href="#">{label}</a></li>
+    <li className={`${styles.skill} ${lato.className}`}><Link href={url}>{label}</Link></li>
   )
 }
 
