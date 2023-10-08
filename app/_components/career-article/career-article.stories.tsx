9890f9dcc6d6061f689763f8303c46ae25cabfaf
Rich Cookson <hellorich@frobitz.com>
2023-10-08 16:32:57 +0100

    Working basic implementation of the CareerArticle component

--- app/_components/career-article/career-article.stories.tsx
+++ app/_components/career-article/career-article.stories.tsx
@@ -1,8 +1,10 @@
-import type { Meta, StoryObj } from "@storybook/react"
-import { within } from "@storybook/testing-library"
-import { expect } from "@storybook/jest"
+/* eslint-disable react/jsx-key */
+import type { Meta, StoryObj } from '@storybook/react'
+import { within } from '@storybook/testing-library'
+import { expect } from '@storybook/jest'
 
-import CareerArticle from "./career-article"
+import CareerArticle from './career-article'
+import Skill from '../skill/skill'
 
 const meta: Meta<typeof CareerArticle> = {
   component: CareerArticle,
@@ -15,11 +17,35 @@ type Story = StoryObj<typeof CareerArticle>
 
 export const Default: Story = {
   args: {
-    children: '',
-    dateEnd: '01-2023',
-    dateStart: '10-2022',
     title: 'Building a single page web app in React for Hugo and Cat',
 		url: 'http://www.hugoandcat.com',
+    dateEnd: '2023-01-01',
+    dateStart: '2022-10-01',
+    children: (
+      <>
+        <p>Front end developer on a project to create a dashboard app, displaying a number of charts and statistics monitoring roll-out and communication of devices for a telecommunications company.</p>
+        <ul>
+          <li>Developed the React app in StoryBook for rapid development while the back-end was being worked on.</li>
+          <li>Used React Query to consume the multiple API&apos;s required by the app.</li>
+          <li>Created a number of charts based on customised Victory Charts components, and also bespoke charts as SVG components.</li>
+          <li>Carried out demonstrations of progress to internal team at Hugo & Cat as well as to stakeholders at the client company thorough sprints.</li>
+        </ul>
+      </>
+    ),
+    skills: [
+      <Skill 
+        text="React" 
+        url="https://reactjs.org"
+      />,
+      <Skill 
+        text="Storybook" 
+        url="https://storybookjs.org"
+      />,
+      <Skill 
+        text="Styled Components" 
+        url="https://styled-components.com"
+      />,
+    ]
   },
 }
 
--- app/_components/career-article/career-article.tsx
+++ app/_components/career-article/career-article.tsx
@@ -1,4 +1,7 @@
+import { ReactNode } from 'react'
 import Link from 'next/link'
+
+import Group from '../group/group'
 
 import { lato } from '../../_utilities/fonts'
 
@@ -8,19 +11,21 @@ interface CareerArticleProps {
   children: any,
   dateEnd: string,
   dateStart: string,
+  skills: ReactNode | ReactNode[],
   title: string,
   url: string,
 }
 
-const CareerArticle = ({ children, dateEnd, dateStart, title, url } : CareerArticleProps) => {
+const CareerArticle = ({ children, dateEnd, dateStart, skills, title, url } : CareerArticleProps) => {
+  const formatDate = (date : string) => new Date(date).toLocaleDateString('en-gb', { year: 'numeric', month: 'short' })
 	return (
 		<article className={`${styles.article} ${lato.className}`}>
       <header className={styles.header}>
-        <h3 className={styles.heading}>{title}</h3>
+        <h3 className={styles.title}>{title}</h3>
         <div className={styles.metadata}>
           <Link href={url}>www.hugoandcat.com</Link>
           <span className={styles.duration}>
-            <time dateTime={dateStart}></time> to <time dateTime={dateEnd}></time>
+            <time dateTime={dateStart}>{formatDate(dateStart)}</time> to <time dateTime={dateEnd}>{formatDate(dateEnd)}</time>
           </span>
         </div>
       </header>
@@ -29,6 +34,11 @@ const CareerArticle = ({ children, dateEnd, dateStart, title, url } : CareerArti
         {children}
       </div>
 
+      <footer>
+        <Group>
+          {skills}
+        </Group>
+      </footer>
       {/* Footer here for skill tags */}
     </article>
 	)
