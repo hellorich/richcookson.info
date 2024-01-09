1b0610a3bfdf3704cf345d72da5b8826da38b577
Rich Cookson <hellorich@frobitz.com>
2024-01-09 12:40:59 +0000

    Converted in page data to JSON to map over - specified Types for data

--- app/(routes)/(home)/page.tsx
+++ app/(routes)/(home)/page.tsx
@@ -2,23 +2,32 @@ import { Header } from '@/app/_components/header'
 import { Group } from '@/app/_components/group'
 import { Skill } from '@/app/_components/skill'
 import { Section } from '@/app/_components/section'
-
 import { CareerArticle } from '@/app/_components/career-article'
 
-import classes from './page.module.scss'
+import skillsData from '@/app/_content/skills.json'
+import articlesData from '@/app/_content/articles.json'
 
 interface Skill {
   text: string;
-  url: URL;
+  url: string;
 }
 
-export default function HomePage() {
-  const skills: Skill[] = [
-    { text: "React", url: new URL('https://reactjs.org') },
-    { text: "Storybook", url: new URL('https://storybookjs.org') },
-    { text: "Styled Components", url: new URL('https://styled-components.com') },
-  ]
+interface Article {
+  title: string;
+  url: string;
+  linkText: string;
+  dateEnd: string;
+  dateStart: string;
+  content: string;
+  skills: Skill[];
+}
 
+const skills: Skill[] = skillsData
+const articles: Article[] = articlesData
+
+import classes from './page.module.scss'
+
+export default function HomePage() {
   return (
     <div className={classes.page}>
       <Header title="Rich Cookson" />
@@ -27,98 +36,13 @@ export default function HomePage() {
         title="Skills"
       >
         <Group>
-          <Skill 
-            text="HTML" 
-            url={new URL("https://developer.mozilla.org/en-US/docs/Web/HTML")}
-          />
-          <Skill 
-            text="CSS" 
-            url={new URL("https://developer.mozilla.org/en-US/docs/Web/CSS")}
-          />
-          <Skill 
-            text="JavaScript" 
-            url={new URL("https://developer.mozilla.org/en-US/docs/Web/JavaScript")}
-          />
-          <Skill 
-            text="React" 
-            url={new URL("https://reactjs.org")}
-          />
-          <Skill 
-            text="Next.js" 
-            url={new URL("https://nextjs.org/")}
-          />
-          <Skill 
-            text="Storybook" 
-            url={new URL("https://storybookjs.org")}
-          />
-          <Skill 
-            text="Styled Components" 
-            url={new URL("https://styled-components.com")}
-          />
-          <Skill 
-            text="Webpack" 
-            url={new URL("https://webpack.js.org")}
-          />
-          <Skill 
-            text="Node" 
-            url={new URL("https://nodejs.org")}
-          />
-          <Skill 
-            text="Express" 
-            url={new URL("https://expressjs.com")}
-          />
-          <Skill 
-            text="Wordpress" 
-            url={new URL("https://wordpress.org")}
-          />
-          <Skill 
-            text="Bootstrap" 
-            url={new URL("http://getbootstrap.com")}
-          />
-          <Skill 
-            text="SCSS" 
-            url={new URL("http://sass-lang.com")}
-          />
-          <Skill 
-            text="jQuery" 
-            url={new URL("https://jquery.com")}
-          />
-          <Skill 
-            text="Git" 
-            url={new URL("https://git-scm.com")}
-          />
-          <Skill 
-            text="SVG" 
-            url={new URL("https://www.w3.org/Graphics/SVG")}
-          />
-          <Skill 
-            text="Docker" 
-            url={new URL("https://www.docker.com")}
-          />
-          <Skill 
-            text="CI" 
-            url={new URL("https://en.wikipedia.org/wiki/Continuous_integration")}
-          />
-          <Skill 
-            text="UNIX" 
-            url={new URL("http://www.opengroup.org/unix")}
-          />
-          <Skill 
-            text="Web Accessibility" 
-            url={new URL("https://www.w3.org/WAI/fundamentals/accessibility-intro")}
-          />
-          <Skill 
-            text="Atomic Design" 
-            url={new URL("http://atomicdesign.bradfrost.com")}
-          />
-          <Skill 
-            text="Responsive Design" 
-            url={new URL("https://en.wikipedia.org/wiki/Responsive_web_design")}
-          />
-          <Skill 
-            text="Figma" 
-            url={new URL("https://www.figma.com")}
-          />
+          {skills.map((skill, index) => (
+            <Skill 
+              key={index}
+              text={skill.text} 
+              url={new URL(skill.url)}
+            />
+          ))}
         </Group>
       </Section>
 
@@ -131,31 +55,24 @@ export default function HomePage() {
           </>
         }
       >
-        <CareerArticle
-          title="Building a single page web app in React for Hugo and Cat"
-          url={new URL('http://www.hugoandcat.com')}
-          linkText="www.hugoandcat.com"
-          dateEnd={new Date('2023, 01, 01')}
-          dateStart={new Date('2022, 10, 01')}
-          content={
-            <>
-              <p>Front end developer on a project to create a dashboard app, displaying a number of charts and statistics monitoring roll-out and communication of devices for a telecommunications company.</p>
-              <ul>
-                <li>Developed the React app in StoryBook for rapid development while the back-end was being worked on.</li>
-                <li>Used React Query to consume the multiple API&apos;s required by the app.</li>
-                <li>Created a number of charts based on customised Victory Charts components, and also bespoke charts as SVG components.</li>
-                <li>Carried out demonstrations of progress to internal team at Hugo & Cat as well as to stakeholders at the client company thorough sprints.</li>
-              </ul>
-            </>
-          }
-          skills={skills.map(skill => (
-            <Skill 
-              key={skill.text}
-              text={skill.text} 
-              url={skill.url}
-            />
-          ))}
-        />
+        {articles.map((article, index) => (
+          <CareerArticle
+            key={index}
+            title={article.title}
+            url={new URL(article.url)}
+            linkText={article.linkText}
+            dateEnd={new Date(article.dateEnd)}
+            dateStart={new Date(article.dateStart)}
+            content={article.content}
+            skills={article.skills.map(skill => (
+              <Skill 
+                key={skill.text}
+                text={skill.text} 
+                url={new URL(skill.url)}
+              />
+            ))}
+          />
+        ))}
       </Section>
     </div>
   )
