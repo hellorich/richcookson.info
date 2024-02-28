996998c6cd55359548fe4562951be7e06b16fa3f
Rich Cookson <hellorich@frobitz.com>
2024-02-28 21:26:27 +0000

    Set up Speed Insights

--- app/(routes)/layout.tsx
+++ app/(routes)/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 export const metadata: Metadata = {
   title: 'Richard Cookson - Front end web developer and designer',
@@ -17,6 +18,7 @@ export default function RootLayout({
       <body>
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   )
